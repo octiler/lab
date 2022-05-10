f5432aa819ce5518069cdbdca1044e732efafa91
octiler <811848512@qq.com>
2022-05-10 08:55:14 +0000

    lit

--- main.js
+++ main.js
@@ -1,48 +1,29 @@
-class MyDiv extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({mode: 'open'})
-        this.shadowRoot.innerHTML = `
-            <style>
-                div {
-                    width:100px; 
-                    height:100px; 
-                    background: green;
-                }
-            </style>
-            <div>
-                <slot></slot>
-            </div>
-        `
-    }
-}
+import {LitElement, html, css} from 'lit'
 
-customElements.define('my-div', MyDiv)
+class LitCounter extends LitElement {
+    
+    static properties = {
+        count: {}
+    }
 
-class Counter extends HTMLElement {
-    static get observedAttributes(){
-        return ['count']
-    }
-    get count(){
-        return this.getAttribute('count')?this.getAttribute('count'):0;
-    }
-    set count(count){
-        this.setAttribute('count', count);
-    }
-    attributeChangedCallback(attr, oldValue, newVal){
-        if(attr == 'count'){
-            this.btn.textContent = newVal;
+    static styles = css`
+        button{
+            color: var(--my-color)
         }
-    }
+    `
+
     constructor(){
-        super();
-        this.attachShadow({mode: 'open'});
-        this.shadowRoot.innerHTML = `
-            <button>${this.count}</button>
-        `
-        this.btn = this.shadowRoot.querySelector('button')
-        this.btn.addEventListener('click', ()=> this.count++)
+        super()
+        this.count = 0;
+    }
+
+    firstUpdated(){
+        // this.count = 999;
+    }
+
+    render(){
+        return html`<button @click=${()=>this.count++}>${this.count}</button>`
     }
 }
 
-customElements.define('my-counter', Counter)
+customElements.define('lit-counter', LitCounter)
